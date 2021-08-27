e9c87bf2e7ed5a73e0366e905d06f85855de7238
Ulises688apaza <ulisesapaza.280@gmail.com>
2021-08-27 09:53:26 -0300

    finished test git

--- 9-Listbox/main.py
+++ 9-Listbox/main.py
@@ -1,3 +1,18 @@
 from tkinter import *
 
 root = Tk()
+root.geometry("500x500")
+
+products = Label(root,  text="Products", font="22")
+products.pack()
+
+listProduct = Listbox(root, width=50)
+listProduct.insert(0, "Some 0")
+listProduct.insert(1, "Some 1")
+listProduct.insert(2, "Some 2")
+listProduct.insert(3, "Some 3")
+listProduct.insert(4, "Some 4")
+listProduct.pack()
+
+
+root.mainloop()
