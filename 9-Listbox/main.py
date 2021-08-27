d6b364f12ecf3bc574a6aca623b1445aa84d60ba
Ulises688apaza <ulisesapaza.280@gmail.com>
2021-08-27 10:41:53 -0300

    add another script about text in tkinter

--- 9-Listbox/main.py
+++ 9-Listbox/main.py
@@ -6,6 +6,9 @@ root.geometry("500x500")
 products = Label(root,  text="Products", font="22")
 products.pack()
 
+def addProduct():
+    listProduct.insert(END, entryOne.get())
+
 listProduct = Listbox(root, width=50)
 listProduct.insert(0, "Some 0")
 listProduct.insert(1, "Some 1")
@@ -14,5 +17,13 @@ listProduct.insert(3, "Some 3")
 listProduct.insert(4, "Some 4")
 listProduct.pack()
 
+# Delete product
+listProduct.delete(0)
+
+entryOne = Entry(root, bd=10)
+entryOne.pack()
+
+btnOne = Button(root, text="Add product", command=addProduct)
+btnOne.pack()
 
 root.mainloop()
